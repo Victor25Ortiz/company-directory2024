3ddef6201aafe023d8b695a358c4c6e46b83127f
Victor25Ortiz <vortiz25@stu.southtexascollege.edu>
2024-02-18 19:38:49 -0600

    finished

--- src/router.js
+++ src/router.js
@@ -9,11 +9,11 @@ import { useAuth } from './composables/useAuth'
 const {isAuthenticated} = useAuth()
 
 const routes = [
-  { path: '/', name: 'Home', component: HomePage },
-  { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
-  { path: '/employees/:id', name: 'CardDetails', component: CardDetails },
-  { path: '/login', name: 'LoginPage', component: LoginPage },
-  { path: '/settings', name: 'SettingsPage', component: SettingsPage, meta: {requiresAuth: true} },
+  { path: '/company-directory2024/', name: 'Home', component: HomePage },
+  { path: '/company-directory2024/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+  { path: '/company-directory2024/employees/:id', name: 'CardDetails', component: CardDetails },
+  { path: '/company-directory2024/login', name: 'LoginPage', component: LoginPage },
+  { path: '/company-directory2024/settings', name: 'SettingsPage', component: SettingsPage, meta: {requiresAuth: true} },
 ]
 
 const router = createRouter({
